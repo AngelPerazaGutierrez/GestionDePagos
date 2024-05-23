1a8e36655844ee10a5a1def7e3db46e327a587ee
Angel <angel.peraza@peaku.io>
2024-05-23 10:33:24 -0500

    get funcionando

--- gestionPagoFront/src/components/perfilUsuario/ComprobanteCard.jsx
+++ gestionPagoFront/src/components/perfilUsuario/ComprobanteCard.jsx
@@ -5,55 +5,55 @@ import { odtenerComprobantes } from "../../services/ComprobantePago";
 import { BsTrashFill } from "react-icons/bs";
 console.log(odtenerComprobantes, "Comprobante");
 export const ComprobanteCard = () => {
-  //   const [informacionCard, setInformacionCard] = useState([]);
+    const [informacionCard, setInformacionCard] = useState([]);
 
-  const informacionCard = [
-    {
-      id: 1,
-      fecha: "2024-05-22",
-      ciudad: "Ciudad A",
-      nit: "123456789",
-      tercero: "Tercero A",
-      concepto: "Concepto A",
-      banco: "Banco A",
-      tipo_cuenta: "Cuenta corriente",
-      numero_cuenta: "123456789",
-      valor: 100,
-    },
-    {
-      id: 1,
-      fecha: "2024-05-22",
-      ciudad: "Ciudad A",
-      nit: "123456789",
-      tercero: "Tercero A",
-      concepto: "Concepto A",
-      banco: "Banco A",
-      tipo_cuenta: "Cuenta corriente",
-      numero_cuenta: "123456789",
-      valor: 100,
-    },
-    {
-      id: 1,
-      fecha: "2024-05-22",
-      ciudad: "Ciudad A",
-      nit: "123456789",
-      tercero: "Tercero A",
-      concepto: "Concepto A",
-      banco: "Banco A",
-      tipo_cuenta: "Cuenta corriente",
-      numero_cuenta: "123456789",
-      valor: 100,
-    },
-  ];
+  // const informacionCard = [
+  //   {
+  //     id: 1,
+  //     fecha: "2024-05-22",
+  //     ciudad: "Ciudad A",
+  //     nit: "123456789",
+  //     tercero: "Tercero A",
+  //     concepto: "Concepto A",
+  //     banco: "Banco A",
+  //     tipo_cuenta: "Cuenta corriente",
+  //     numero_cuenta: "123456789",
+  //     valor: 100,
+  //   },
+  //   {
+  //     id: 1,
+  //     fecha: "2024-05-22",
+  //     ciudad: "Ciudad A",
+  //     nit: "123456789",
+  //     tercero: "Tercero A",
+  //     concepto: "Concepto A",
+  //     banco: "Banco A",
+  //     tipo_cuenta: "Cuenta corriente",
+  //     numero_cuenta: "123456789",
+  //     valor: 100,
+  //   },
+  //   {
+  //     id: 1,
+  //     fecha: "2024-05-22",
+  //     ciudad: "Ciudad A",
+  //     nit: "123456789",
+  //     tercero: "Tercero A",
+  //     concepto: "Concepto A",
+  //     banco: "Banco A",
+  //     tipo_cuenta: "Cuenta corriente",
+  //     numero_cuenta: "123456789",
+  //     valor: 100,
+  //   },
+  // ];
 
-  //   useEffect(() => {
-  //     const getData = async () => {
-  //       const data = await odtenerComprobantes();
-  //       setInformacionCard(data);
-  //     };
+    useEffect(() => {
+      const getData = async () => {
+        const data = await odtenerComprobantes();
+        setInformacionCard(data);
+      };
 
-  //     getData();
-  //   }, []);
+      getData();
+    }, []);
   return (
     <div className="containerCard py-4   rounded  container mx-auto gap-1 w-full hidden lg:flex  justify-center items-center mb-4">
       {informacionCard.map((transaccion) => (
