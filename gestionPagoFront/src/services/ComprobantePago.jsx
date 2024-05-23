8bcabb478ef039ec3f49b5d49c4fa8c16bff58ad
Angel <angel.peraza@peaku.io>
2024-05-22 20:44:05 -0500

    añadiendo get al crud de comprobante de pago

--- gestionPagoFront/src/services/ComprobantePago.jsx
+++ gestionPagoFront/src/services/ComprobantePago.jsx
@@ -1,6 +1,21 @@
 import axios from "axios";
 import { GetBackendURL } from "../config/GetBackendURL";
 
+export const consultarComprobante = async (datosFormIngreso) => {
+  console.log(datosFormIngreso);
+  try {
+    const response = await axios.post(
+      `${GetBackendURL}cp/cp-principal`,
+      datosFormIngreso
+    );
+    console.log(`mostrando comprobantes de pago`, response);
+  } catch (error) {
+    console.error("Error al consultar los comprobantes de pago:", error);
+  }
+};
+
+
+
 export const crearComprobante = async (datosFormIngreso) => {
   console.log(datosFormIngreso);
   try {
