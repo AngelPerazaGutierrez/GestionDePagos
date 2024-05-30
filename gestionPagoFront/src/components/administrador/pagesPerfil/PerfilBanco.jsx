56dde9ab530a7d8cd70dc0f556d5431ac6124ec7
torco34 <torcoromaarias@gmail.com>
2024-05-29 21:21:54 -0500

    logica button

--- gestionPagoFront/src/components/administrador/pagesPerfil/PerfilBanco.jsx
+++ gestionPagoFront/src/components/administrador/pagesPerfil/PerfilBanco.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormCreateBanco } from "../../form/FormCreateBanco";
 import Container from "react-bootstrap/Container";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { CustomButton } from "../../common/CustomButton";
+import { CompoBanco } from "../CompoBanco";
 export const PerfilBanco = () => {
+  const [showBancos, setShowBancos] = useState(null);
+  const handleShowBancos = () => {
+    setShowBancos(!showBancos);
+    alert("jrrnfjer");
+  };
   return (
     <div>
       <Row>
@@ -14,7 +20,8 @@ export const PerfilBanco = () => {
         </Col>
         <Col xs={!2} md={6}>
           <p>Visualizar Bancos</p>
-          <CustomButton text="Visualizar bancos" />
+          <CustomButton text="Visualizar bancos" onClick={handleShowBancos} />
+          {showBancos && <CompoBanco />}
         </Col>
       </Row>
     </div>
